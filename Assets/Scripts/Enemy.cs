ec17cb22266c179c21e40387193553183be5427d
AtsushiUemura <atsushi.git@gmail.com>
2016-06-17 14:13:40 +0900

    second commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public abstract class Enemy : MonoBehaviour
+{
+
+    protected int maxHp { get; set; }
+    protected int hp { get; set; }
+    protected int speed { get; set; }
+    protected int attack { get; set; }
+    protected int deffence { get; set; }
+    protected int addScore { get; set; }
+    protected bool isDead { get; set; }
+    protected Slider hpBar { get; set; }
+
+    protected abstract void Move();
+    protected abstract void Shot();
+
+    protected void OnBecameInvisible()
+    {
+        // ScoreManager.instance.AddScore(addScore);
+        gameObject.SetActive(false);
+    }
+    public void Damage(int damage)
+    {
+        hp -= damage;
+        if (hp <= 0)
+        {
+            // ScoreManager.instance.StopCombo();
+            isDead = true;
+            gameObject.SetActive(false);
+        }
+    }
+    protected void InitStatus()
+    {
+        hp = maxHp;
+    }
+}
