50b450fd3037d1eb04075965890cd273044049e6
AtsushiUemura <atsushi.git@gmail.com>
2016-06-17 20:48:17 +0900

    黒魔法少女実装中

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -5,7 +5,7 @@ public class AI : MonoBehaviour
 {
 
     public void Tracking(Transform targetPos)
-    {        
+    {
         Vector2 direction = targetPos.position - gameObject.transform.position;
         direction.Normalize();
         Vector2 pos = transform.position;
@@ -13,16 +13,14 @@ public class AI : MonoBehaviour
         transform.position = pos;
     }
 
-
-    // Use this for initialization
-    void Start()
+    public void Intercept(Transform targetPos)
     {
-
+        float vr = targetPos.position.normalized.magnitude - transform.position.normalized.magnitude;
+        float sr = Vector2.Distance(targetPos.position, transform.position);
+        float tc = Mathf.Abs(sr) / Mathf.Abs(vr);
+        Vector2 pos = targetPos.position + (targetPos.position.normalized - transform.position.normalized) * tc;
+        pos = pos.normalized  * Time.deltaTime;
+        transform.position = pos;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
