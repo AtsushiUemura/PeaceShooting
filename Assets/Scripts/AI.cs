eba2de65e0728986b27f851f8bab8e04c8b8c729
AtsushiUemura <atsushi.git@gmail.com>
2016-06-17 20:05:57 +0900

    黒魔法少女実装中

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour
+{
+
+    public void Tracking(Transform targetPos)
+    {        
+        Vector2 direction = targetPos.position - gameObject.transform.position;
+        direction.Normalize();
+        Vector2 pos = transform.position;
+        pos += direction * Time.deltaTime;
+        transform.position = pos;
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
